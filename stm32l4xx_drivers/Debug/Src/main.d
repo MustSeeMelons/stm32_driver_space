6f1efc82b0168e468c7dfb284e56c5c2f3b53b94
strautins <strautins.janis@gmail.com>
2024-06-23 12:25:16 +0300

    On board button led toggle

--- stm32l4xx_drivers/Debug/Src/main.d
+++ stm32l4xx_drivers/Debug/Src/main.d
@@ -1,7 +1,7 @@
 Src/main.o: ../Src/main.c \
- C:/STM32_Drivers/stm32l4xx_drivers/drivers/Inc/stm32l476xx.h \
- C:/STM32_Drivers/stm32l4xx_drivers/drivers/Inc/stm32l476xx_gpio_driver.h \
- C:/STM32_Drivers/stm32l4xx_drivers/drivers/Inc/stm32l476xx.h
-C:/STM32_Drivers/stm32l4xx_drivers/drivers/Inc/stm32l476xx.h:
-C:/STM32_Drivers/stm32l4xx_drivers/drivers/Inc/stm32l476xx_gpio_driver.h:
-C:/STM32_Drivers/stm32l4xx_drivers/drivers/Inc/stm32l476xx.h:
+ /Users/strautins/git/stm32_driver_space/stm32l4xx_drivers/drivers/Inc/stm32l476xx.h \
+ /Users/strautins/git/stm32_driver_space/stm32l4xx_drivers/drivers/Inc/stm32l476xx_gpio_driver.h \
+ /Users/strautins/git/stm32_driver_space/stm32l4xx_drivers/drivers/Inc/stm32l476xx.h
+/Users/strautins/git/stm32_driver_space/stm32l4xx_drivers/drivers/Inc/stm32l476xx.h:
+/Users/strautins/git/stm32_driver_space/stm32l4xx_drivers/drivers/Inc/stm32l476xx_gpio_driver.h:
+/Users/strautins/git/stm32_driver_space/stm32l4xx_drivers/drivers/Inc/stm32l476xx.h:
--- stm32l4xx_drivers/Src/main.c
+++ stm32l4xx_drivers/Src/main.c
@@ -26,65 +26,104 @@
 //#endif
 
 void led_push_pull() {
-	// Setup GPIO
-	GPIO_Handle_t gpio_handle = {
-		.pGPIOx = GPIOA,
-		.GPIO_PinConfig = {
-			.GPIO_PinNumber = GPIO_PIN_N5,
-			.GPIO_PinMode = GPIO_MODE_OUTPUT,
-			.GPIO_PinSpeed = GPIO_SPEED_FAST,
-			.GPIO_PinPuPdControl = GPIO_NO_PUPD,
-			.GPIO_PinOPType = GPIO_OP_TYPE_PP,
-		}
-	};
+    // Setup GPIO
+    GPIO_Handle_t gpio_handle = { .pGPIOx = GPIOA, .GPIO_PinConfig = {
+            .GPIO_PinNumber = GPIO_PIN_N5, .GPIO_PinMode = GPIO_MODE_OUTPUT,
+            .GPIO_PinSpeed = GPIO_SPEED_FAST, .GPIO_PinPuPdControl =
+                    GPIO_NO_PUPD, .GPIO_PinOPType = GPIO_OP_TYPE_PP, } };
 
-	// Enable clock for port A
-	GPIO_PCLK(GPIOA, ENABLE);
+    // Enable clock for port A
+    GPIO_PCLK(GPIOA, ENABLE);
 
-	GPIO_Init(&gpio_handle);
+    GPIO_Init(&gpio_handle);
 
-	while (1) {
-		// Write to GPIO
-		GPIO_TogglePin(GPIOA, gpio_handle.GPIO_PinConfig.GPIO_PinNumber);
+    while (1) {
+        // Write to GPIO
+        GPIO_TogglePin(GPIOA, gpio_handle.GPIO_PinConfig.GPIO_PinNumber);
 
-		// Locking delay
-		for(uint32_t i=0;i<500000;i++);
-	}
+        // Locking delay
+        for (uint32_t i = 0; i < 500000; i++)
+            ;
+    }
 }
 
 // Very dim, pull up is ~40k Ohm, needs external small pull up
 void led_open_drain() {
-	// Setup GPIO
-	GPIO_Handle_t gpio_handle = {
-		.pGPIOx = GPIOA,
-		.GPIO_PinConfig = {
-			.GPIO_PinNumber = GPIO_PIN_N5,
-			.GPIO_PinMode = GPIO_MODE_OUTPUT,
-			.GPIO_PinSpeed = GPIO_SPEED_FAST,
-			.GPIO_PinPuPdControl = GPIO_PIN_PU,
-			.GPIO_PinOPType = GPIO_OP_TYPE_OD,
-		}
-	};
+    // Setup GPIO
+    GPIO_Handle_t gpio_handle = { .pGPIOx = GPIOA, .GPIO_PinConfig = {
+            .GPIO_PinNumber = GPIO_PIN_N5, .GPIO_PinMode = GPIO_MODE_OUTPUT,
+            .GPIO_PinSpeed = GPIO_SPEED_FAST,
+            .GPIO_PinPuPdControl = GPIO_PIN_PU, .GPIO_PinOPType =
+                    GPIO_OP_TYPE_OD, } };
 
-	// Enable clock for port A
-	GPIO_PCLK(GPIOA, ENABLE);
+    // Enable clock for port A
+    GPIO_PCLK(GPIOA, ENABLE);
 
-	GPIO_Init(&gpio_handle);
+    GPIO_Init(&gpio_handle);
 
-	while (1) {
-		// Write to GPIO
-		GPIO_TogglePin(GPIOA, gpio_handle.GPIO_PinConfig.GPIO_PinNumber);
+    while (1) {
+        // Write to GPIO
+        GPIO_TogglePin(GPIOA, gpio_handle.GPIO_PinConfig.GPIO_PinNumber);
 
-		// Locking delay
-		for(uint32_t i=0;i<500000;i++);
-	}
+        // Locking delay
+        for (uint32_t i = 0; i < 500000; i++)
+            ;
+    }
 }
 
+// Button is on PC13
 void led_button_toggle() {
-	// TODO implement
+    GPIO_Handle_t led_handle = {
+        .pGPIOx = GPIOA,
+        .GPIO_PinConfig = {
+                .GPIO_PinNumber = GPIO_PIN_N5,
+                .GPIO_PinMode = GPIO_MODE_OUTPUT,
+                .GPIO_PinSpeed = GPIO_SPEED_FAST,
+                .GPIO_PinPuPdControl = GPIO_NO_PUPD,
+                .GPIO_PinOPType = GPIO_OP_TYPE_PP,
+        }
+    };
+
+    GPIO_Handle_t button_handle = {
+        .pGPIOx = GPIOC,
+        .GPIO_PinConfig = {
+                .GPIO_PinNumber = GPIO_PIN_N13,
+                .GPIO_PinMode = GPIO_MODE_INPUT,
+                .GPIO_PinSpeed = GPIO_SPEED_FAST,
+                .GPIO_PinPuPdControl = GPIO_PIN_PU,
+        }
+    };
+
+    // Enable clocks!
+    GPIO_PCLK(GPIOA, ENABLE);
+    GPIO_PCLK(GPIOC, ENABLE);
+
+    // Init!
+    GPIO_Init(&led_handle);
+    GPIO_Init(&button_handle);
+
+    uint8_t is_down = 0;
+
+    while (1) {
+        uint8_t button_state = GPIO_ReadFromInputPin(GPIOC, button_handle.GPIO_PinConfig.GPIO_PinNumber);
+
+        if (button_state == 0 && is_down == 0) {
+            is_down = 1;
+            // Debounce
+            for (uint32_t i = 0; i < 50000; i++)
+                ;
+
+            button_state = GPIO_ReadFromInputPin(GPIOC, button_handle.GPIO_PinConfig.GPIO_PinNumber);
+
+            if (button_state == 0) {
+                GPIO_TogglePin(GPIOA, led_handle.GPIO_PinConfig.GPIO_PinNumber);
+            }
+        } else if (button_state == 1) {
+            is_down = 0;
+        }
+    }
 }
 
-int main(void)
-{
-	led_push_pull();
+int main(void) {
+    led_button_toggle();
 }
